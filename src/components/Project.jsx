3dcded5093f016ca2ce758ca1d3d1aa87c5bc1f6
Lindsay Terry <lindsaytee66@gmail.com>
2024-09-02 15:05:00 -0600

    Adds functionality to buttons to complete functionality for custom carousel

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,47 +1,31 @@
-import { Card, CardHeader, CardBody, CardFooter, Image, Text } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Card, CardHeader, CardBody, CardFooter, Image, Text, Box } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
 
-export default function Project() {
-    const [projects, setProjects] = useState(
-        [
-            {
-                projectTitle: 'The Stock-Ness Monster', 
-                imageSrc: '/images/stockness.png', 
-                projectDescription: 'An inventory management application designed to maintain data integrity.',
-                link: 'https://github.com/lindsay-terry/stock-ness-monster',
-            },
-            {
-                projectTitle: 'Blog City Tech',
-                imageSrc: '/images/blogcitytech.png',
-                projectDescription: 'A CMS-style blog site utilizing the MVC paradigm, Handlebars.js for templating, Express.js for API routes, and Sequelize to connect with a PostgreSQL database.',
-                link: 'https://github.com/lindsay-terry/blog-city-tech'
-            },
-            {
-                projectTitle: "What's for Dinner?",
-                imageSrc: '/images/whatsfordinner.png',
-                projectDescription: 'A front-end application using two server-side APIs to generate meal and drink recipes.',
-                link: 'https://github.com/lindsay-terry/whats-for-dinner'
-            }
-        ]
-    );
+export default function Project({projects, projectIndex}) {
+
+        // Use props to determine which project to display
+        const project = projects[projectIndex];
 
     return (
-        <Card maxW='sm'>
-            {/* <CardHeader> Project Title</CardHeader>
-            <CardBody>
-                <Image src='/images/stockness.png' alt='screenshot of application' borderRadius='lg'/>
-                    
-                <Text>
-                    <p>Project description</p>
-                </Text>
-            </CardBody>
-            <CardFooter>
-                Footer
-            </CardFooter> */}
-            {projects.map((project) => {
-                console.log(project);
-            })}
-        </Card>
-    
+        <Box>
+            <Card maxW='sm'>
+                <CardHeader> {project.projectTitle}</CardHeader>
+                <CardBody>
+                    <Image src={project.imageSrc} alt='screenshot of project' borderRadius='lg'/>
+                    <Text>
+                        {project.projectDescription}
+                    </Text>
+                </CardBody>
+                <CardFooter>
+                    Footer
+                </CardFooter>
+            </Card>
+        </Box>
     );
-}
+};
+
+// Using propTypes on projects and projectIndex to ensure an array and a number are required
+Project.propTypes = {
+    projects: PropTypes.array.isRequired,
+    projectIndex: PropTypes.number.isRequired,
+};
--- src/endpoints/Portfolio.jsx
+++ src/endpoints/Portfolio.jsx
@@ -10,10 +10,33 @@ export default function Portfolio() {
             borderRadius: '30px'
         }
     }
-  
+
+    // Array of projects to show on portfolio page
+    const projects = [
+        {
+            projectTitle: 'The Stock-Ness Monster', 
+            imageSrc: '/images/stockness.png', 
+            projectDescription: 'An inventory management application designed to maintain data integrity.',
+            link: 'https://github.com/lindsay-terry/stock-ness-monster',
+        },
+        {
+            projectTitle: 'Blog City Tech',
+            imageSrc: '/images/blogcitytech.png',
+            projectDescription: 'A CMS-style blog site utilizing the MVC paradigm, Handlebars.js for templating, Express.js for API routes, and Sequelize to connect with a PostgreSQL database.',
+            link: 'https://github.com/lindsay-terry/blog-city-tech'
+        },
+        {
+            projectTitle: "What's for Dinner?",
+            imageSrc: '/images/whatsfordinner.png',
+            projectDescription: 'A front-end application using two server-side APIs to generate meal and drink recipes.',
+            link: 'https://github.com/lindsay-terry/whats-for-dinner'
+        }
+    ];
+
     return (
         <Container p={7} my={2} style={styles.customBackground}>
-            <Carousel />
+            {/* Pass projects as props to carousel component */}
+            <Carousel projects={projects}/>
         </Container>
     );
 }
